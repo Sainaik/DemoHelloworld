a912d0a11020667a0f613c3f407f3ca5d503c26a
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-24 10:07:12 +0530

    first commit

---
+++ HelloWorld/Add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Add
+    {
+        /// <summary>
+        /// Adds the nums.
+        /// </summary>
+        /// <param name="a">a.</param>
+        /// <param name="b">The b.</param>
+        /// <returns></returns>
+       
+        public int addNums(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Add add = new Add();
+
+            int b = add.addNums(5, 10);
+
+            Console.WriteLine(b);
+        }
+    }
+}
